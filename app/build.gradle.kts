6a26e40adeb98a1ac28a8f96d87dcdc5e28fdd1f
Nagma Bano <nagma.bano@sap.com>
2024-02-12 14:08:39 +0530

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,15 @@ android {
 
 dependencies {
 
+    // Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0:2.7.0")
+    // Network Call
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Json to Kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Image Loading
+    implementation("io.coil-kt:coil-compose:2.4.0 ")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
